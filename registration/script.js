8c2b1e21ed1965c9059b02d635f70e455d007661
Temryakov <therevors@gmail.com>
2020-10-16 16:23:56 +0300

    added hamburger#

--- registration/script.js
+++ registration/script.js
@@ -1,8 +1,9 @@
 function validateForm () {
-    var checklogin = document.forms['registration_form']['login'].value;
-    var checkname = document.forms['registration_form']['name'].value;
-    var checkpassword = document.forms['registration_form']['password'].value;
-    var checkconfirmpassword = document.forms['registration_form']['confirm_password'].value;
+    
+    let checklogin = document.forms['registration_form']['login'].value;
+    let checkname = document.forms['registration_form']['name'].value;
+    let checkpass = document.forms['registration_form']['password'].value;
+    let checkconfpass = document.forms['registration_form']['confirm_password'].value;
 
     if (checklogin == "") {
         document.getElementById('empty_value').innerHTML = 'Вы не ввели логин.'
@@ -12,19 +13,19 @@ function validateForm () {
         document.getElementById('empty_value').innerHTML = 'Вы не ввели имя.'
         return false;
     }
-    else if (checkpassword == "") {
+    else if (checkpass == "") {
         document.getElementById('empty_value').innerHTML = 'Вы не ввели пароль.'
         return false;
     }
-    else if (checkconfirmpassword == "") {
+    else if (checkconfpass == "") {
         document.getElementById('empty_value').innerHTML = 'Вы не повторили пароль.'
         return false;
     }
-    else if (checkpassword.length < 6) {
+    else if (checkpass.length < 6) {
         document.getElementById('empty_value').innerHTML = 'Пароль должен содержать более 6 символов\n и не содержать пробелы.'
         return false;
     }
-    else if (checkpassword !== checkconfirmpassword) {
+    else if (checkpass !== checkconfpass) {
         document.getElementById('empty_value').innerHTML = 'Пароли не совпадают.'
         return false;
     }
